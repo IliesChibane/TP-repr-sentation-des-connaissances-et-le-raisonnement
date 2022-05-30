52b3d7b3866a89e2fd4d93ea172214069ea07952
Rania Bourouina <bourouinarania@gmail.com>
2022-05-30 17:25:05 +0100

    still tp5

---
+++ TP3/maven.rcrtp3/src/main/java/maven/rcrtp3/app1.java
@@ -0,0 +1,13 @@
+package maven.rcrtp3;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+import a.e;
+import be.fnord.util.logic.DefaultReasoner;
+import be.fnord.util.logic.WFF;
+import be.fnord.util.logic.defaultLogic.DefaultRule;
+import be.fnord.util.logic.defaultLogic.RuleSet;
+import be.fnord.util.logic.defaultLogic.WorldSet;
+public class app1 {
+
+}
